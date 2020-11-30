fef3130459f645f93ced007fad54125281aaf64c
Anders <Anders@DESKTOP-KV5N0NE>
2020-11-30 17:56:12 +0400

    30.11.2020

---
+++ SpringMVC/SpringMVC/src/main/java/com/mycompany/dao/PersonDAO.java
@@ -0,0 +1,27 @@
+package com.mycompany.dao;
+
+import com.mycompany.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private List<Person> people;
+    private static int PEOPLE_COUNT;
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Thomas Anders"));
+        people.add(new Person(++PEOPLE_COUNT, "Diter Bolen"));
+        people.add(new Person(++PEOPLE_COUNT, "Freddie Mercury"));
+        people.add(new Person(++PEOPLE_COUNT, "Till Lindemann"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+    public Person show(int id) {
+        return people.stream().filter(person->person.getId() == id).findAny().orElse(null);
+    }
+}
