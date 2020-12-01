19dd4eac0408104e754f180a351505c42709451d
Anders <Anders@DESKTOP-KV5N0NE>
2020-12-01 17:33:31 +0400

    01.12.2020

--- SpringMVC/SpringMVC/src/main/java/com/mycompany/models/Person.java
+++ SpringMVC/SpringMVC/src/main/java/com/mycompany/models/Person.java
@@ -1,7 +1,12 @@
 package com.mycompany.models;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Person {
     private int id;
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
 
     public Person() {
