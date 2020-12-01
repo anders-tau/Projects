65bcbf939deab09f79b7d5a6c91eac48554ba512
Anders <Anders@DESKTOP-KV5N0NE>
2020-12-01 17:03:30 +0400

    01.12.2020

--- SpringMVC/SpringMVC/src/main/java/com/mycompany/dao/PersonDAO.java
+++ SpringMVC/SpringMVC/src/main/java/com/mycompany/dao/PersonDAO.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class PersonDAO {
     private List<Person> people;
     private static int PEOPLE_COUNT;
+
     {
         people = new ArrayList<>();
         people.add(new Person(++PEOPLE_COUNT, "Thomas Anders"));
@@ -18,14 +19,27 @@ public class PersonDAO {
         people.add(new Person(++PEOPLE_COUNT, "Till Lindemann"));
     }
 
-    public List<Person> index() {
+    public List<Person> index()
+    {
         return people;
     }
-    public Person show(int id) {
+
+    public Person show(int id)
+    {
         return people.stream().filter(person->person.getId() == id).findAny().orElse(null);
     }
-    public void save(Person person) {
+
+    public void save(Person person)
+    {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
+    }
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+        personToBeUpdated.setName(updatedPerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p->p.getId() == id);
     }
 }
