25522acdad4866d6bbc4b258c7b2dbe6dec7b8f6
Anders <Anders@DESKTOP-KV5N0NE>
2020-12-05 22:15:37 +0400

    JNDI,MySQL

--- SpringMVC/SpringMVC/src/main/java/com/mycompany/controllers/PeopleController.java
+++ SpringMVC/SpringMVC/src/main/java/com/mycompany/controllers/PeopleController.java
@@ -26,26 +26,31 @@ public class PeopleController {
         model.addAttribute("people", personDAO.index());
         return "people/index";
     }
+
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
     }
+
     @GetMapping("/new")
     public String newPerson(@ModelAttribute("person") Person person) {
         return "people/new";
     }
+
     @PostMapping()
     public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult){
         if (bindingResult.hasErrors()) return "people/new";
         personDAO.save(person);
         return "redirect:/people";
     }
+
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id){
         model.addAttribute("person", personDAO.show(id));
         return "people/edit";
     }
+
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
                          @PathVariable("id") int id) {
@@ -53,6 +58,7 @@ public class PeopleController {
         personDAO.update(id, person);
         return "redirect:/people";
     }
+
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") int id) {
         personDAO.delete(id);
--- SpringMVC/SpringMVC/src/main/java/com/mycompany/dao/PersonDAO.java
+++ SpringMVC/SpringMVC/src/main/java/com/mycompany/dao/PersonDAO.java
@@ -3,43 +3,117 @@ package com.mycompany.dao;
 import com.mycompany.models.Person;
 import org.springframework.stereotype.Component;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
-    private List<Person> people;
-    private static int PEOPLE_COUNT;
 
-    {
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Thomas Anders"));
-        people.add(new Person(++PEOPLE_COUNT, "Diter Bolen"));
-        people.add(new Person(++PEOPLE_COUNT, "Freddie Mercury"));
-        people.add(new Person(++PEOPLE_COUNT, "Till Lindemann"));
+    private static Connection connection;
+
+    static {
+        InitialContext context = null;
+        try {
+            context = new InitialContext();
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+        DataSource ds = null;
+        try {
+            assert context != null;
+            ds = (DataSource) context.lookup("java:/comp/env/jdbc/test");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+        try {
+            assert ds != null;
+            connection = ds.getConnection();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
-    public List<Person> index()
-    {
+    public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "select * from person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+            while (resultSet.next()) {
+                Person person = new Person();
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setEmail(resultSet.getString("email"));
+                people.add(person);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return people;
     }
 
-    public Person show(int id)
-    {
-        return people.stream().filter(person->person.getId() == id).findAny().orElse(null);
+    public Person show(int id) {
+        Person person = new Person();
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "select * from person where id = " + id;
+            ResultSet resultSet = statement.executeQuery(SQL);
+            if (resultSet.next()) {
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setEmail(resultSet.getString("email"));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return person;
     }
 
-    public void save(Person person)
-    {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+    public void save(Person person) {
+        try {
+            Statement statement = connection.createStatement();
+            String SQLmaxId = "select max(id) as maxid from person";
+            ResultSet resultSet = statement.executeQuery(SQLmaxId);
+            int maxId = 0;
+            if (resultSet.next()) {
+                maxId = resultSet.getInt("maxid");
+            }
+            if (maxId != 0) {
+                maxId = maxId + 1;
+                String SQL = "insert into person values(" + maxId + ",'" + person.getName() +
+                        "'," + person.getAge() + ",'" + person.getEmail() + "')";
+                statement.executeUpdate(SQL);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
+
     public void update(int id, Person updatedPerson) {
-        Person personToBeUpdated = show(id);
-        personToBeUpdated.setName(updatedPerson.getName());
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "update person set name = '" + updatedPerson.getName() +
+                         "', age = " + updatedPerson.getAge() + ", email = '" +
+                         updatedPerson.getEmail() + "' where id = " + id;
+            statement.executeUpdate(SQL);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void delete(int id) {
-        people.removeIf(p->p.getId() == id);
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "delete from Person where id = " + id;
+            statement.executeUpdate(SQL);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
