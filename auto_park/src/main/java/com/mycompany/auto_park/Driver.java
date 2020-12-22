548010d4e485dfd4d027a4ee206e1baf4c216b48
Anders <Anders@DESKTOP-KV5N0NE>
2020-12-22 19:37:40 +0400

    22.12.2020

--- auto_park/src/main/java/com/mycompany/auto_park/Driver.java
+++ auto_park/src/main/java/com/mycompany/auto_park/Driver.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.auto_park;
 
 import java.util.Set;
 import java.util.HashSet;
 
-/**
- *
- * @author Anders
- */
 public class Driver {
     private Long id;
     private String name;
